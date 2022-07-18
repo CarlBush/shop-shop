d7d0fa4209e062c90ea723a8706b2c9287fb329a
Carl Bush <CarlBushs13@gmail.com>
2022-07-18 09:33:13 -0700

    UPDATE_CATEGORIES test

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,11 +1,42 @@
+import { reducer } from "../utils/reducers";
+
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY
 } from "../utils/actions";
 
-const initalState = {
+const initialState = {
     products: [],
     categories: [{ name: "Food"}],
     currentCategory: "1",
 };
+
+
+/*
+This function accepts the following two parameters:
+The current state object, so we can make our copy of it for the new state.
+The action we're performing to update state, which is broken into the following two parts as an object:
+*/
+//UPDATE_PRODUCTS create a new state object (products)
+test("UPDATE_PRODUCTS", () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products: [{}, {}]
+    });
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
+
+
+
+test("UPDATE_CATEGORIES", () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
